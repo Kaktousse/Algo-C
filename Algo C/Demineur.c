57aa0a8b3620448937da4bf31f8de413c5931841
Kaktousse <147042560+Kaktousse@users.noreply.github.com>
2024-03-18 13:28:59 +0100

    vvv

--- Algo C/Demineur.c
+++ Algo C/Demineur.c
@@ -1,14 +1,9 @@
 ﻿#include <stdlib.h> 
 #include <time.h> 
 #include <stdio.h> 
+#include<conio.h>
 
 
-typedef struct Grid
-{
-    int size;
-    int Bomb;
-    int flag;
-};
 
 typedef struct Tile
 {
@@ -19,29 +14,30 @@ typedef struct Tile
 
 } Tile;
 
+typedef struct Grid
+{
+    Tile** tiles;
+    int size;
+    int Bomb;
+    int flag;
+} Grid;
+
+
 void ClearBuffer()
 {
     while (getchar() != '\n');
 }
 
-void ChangeTile(Tile** grid, int lign, int column) {
-    grid[lign][column].revealed = 1;
-    
-}
 
-void SetBombAround(Tile** grid, int setlign, int setcolumn, int size) {
-    for (int lign = -1; lign <= 1; lign++) {
-        for (int column = -1; column <= 1; column++) {
-            if ((lign != 0 && column != 0) && (setlign+lign > -1 && setlign+lign < size) && (setcolumn + column > -1 && setcolumn + column < size) ) {
-                if (grid[setlign + lign][setcolumn + column].Bomb == 1) {
-                    grid[setlign][setcolumn].BombAround += 1;
-                }
-            }
+void SetBombAround(Grid grid, int setlign, int setcolumn, int size) {
+    for (int lign = -1; lign < 2; lign++){
+        for (int column = -1; column <2; column++){
+            grid.tiles[setlign+lign][setcolumn+column].BombAround +=1;
         }
     }
 }
 
-void AskCoor(Tile** grid) {
+void AskCoor(Grid grid, int firststart) {
     int lign, column;
     printf("Choose the lign : ");
     scanf_s("%d", &lign);
@@ -50,40 +46,40 @@ void AskCoor(Tile** grid) {
     scanf_s("%d", &column);
     ClearBuffer();
 
-    ChangeTile(grid, lign, column);
-    CheckTile(grid, lign, column);
+    if (firststart){
+        PlaceBomb(grid);
+    }
+    grid.tiles[lign][column].revealed = 1;
+
+    CheckTile(grid, lign, column, 1);
 
 }
 
 
-int CheckTile(Tile** grid, int lign, int column) {
+int CheckTile(Grid grid, int lign, int column, int expand) {
     return 1;
 }
 
 
 
-int** PlaceBomb(Tile** grid, int size, int bombnumber) {
-    
+int** PlaceBomb(Grid grid) {
+    int size = grid.size;
+
     int place = 0;
     int* bomb = (int*) malloc(sizeof(int)*2);
-    for (int count = 0; count < bombnumber; count++) {
+    for (int count = 0; count < grid.Bomb; count++) {
         while (!place) {
             bomb[0] = rand() % size;
             bomb[1] = rand() % size;
-            if (grid[bomb[0]][bomb[1]].Bomb == 0) {
-                grid[bomb[0]][bomb[1]].Bomb = 1;
+            if (grid.tiles[bomb[0]][bomb[1]].Bomb == 0) {
+                grid.tiles[bomb[0]][bomb[1]].Bomb = 1;
                 place = 1;
+                SetBombAround(grid, bomb[0], bomb[1], size);
             }
         }
         place = 0;
     }
     
-    for (int lign = 0; lign < size; lign++) {
-        for (int column = 0; column < size; column++) {
-            SetBombAround(grid, lign, column, size);
-        }
-    }
-
 }
 
 
@@ -91,7 +87,7 @@ Tile** InitGrid(int givensize) {
     
     int x, y; 
     Tile** grid;
-
+    
     grid = (Tile*)malloc(sizeof(Tile) * givensize);
 
     for (int count = 0; count < givensize; count++) {
@@ -108,10 +104,13 @@ Tile** InitGrid(int givensize) {
 }
 
 
-void PrintGrid(Tile** grid, int size) {
+void PrintGrid(Grid grid) {
+
+    int size = grid.size;
+    printf("Press F to place a flag");
     printf("\t");
     for (int i = 0; i < size; i++) {
-        if (i < 10) { printf("   %d", i);  } else { printf("  %d", i); }
+        if (i < 10) { printf("   0%d", i);  } else {printf("   %d,i");}
        
     }
     printf("\n");
@@ -120,13 +119,13 @@ void PrintGrid(Tile** grid, int size) {
 
         printf(" %d \t", lign);
         for (int column = 0; column < size; ++column) {
-            if (grid[lign][column].revealed == 0) {
+            if (grid.tiles[lign][column].revealed == 0) {
                 printf(" | []");
-            }else if (grid[lign][column].BombAround == 0) {
+            }else if (grid.tiles[lign][column].BombAround == 0) {
                 printf(" |   "); 
             }
             else {
-                printf(" | %d", grid[lign][column].BombAround);
+                printf(" | %d ", grid.tiles[lign][column].BombAround);
             }
             
         }
@@ -140,13 +139,22 @@ int main() {
     struct Grid myGrid;
     myGrid.size = 10;
     myGrid.Bomb = 10;
-
-
     srand(time(NULL));
-    Tile** grid = InitGrid(myGrid.size);
-    PlaceBomb(grid, myGrid.size, myGrid.Bomb);
-    PrintGrid(grid, myGrid.size);
-    AskCoor(grid);
-    PrintGrid(grid, myGrid.size);
+    int firststart = 1;
+
+    myGrid.tiles = InitGrid(myGrid.size);
+    PrintGrid(myGrid);
+    //first round
+    AskCoor(myGrid, firststart);
+    PrintGrid(myGrid);
+
+    if (kbhit){
+        char ch = getch();
+        if(ch = "E"){
+            printf("ssss");
+            PlaceFlag();
+        }
+    }
+
     return 1;
 }
